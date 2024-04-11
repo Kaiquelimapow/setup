db6bb404061910e4fa3891c3fafb1a58808d1c02
KaiqueLimapow <kaiquelima1011@gmail.com>
2024-04-11 11:32:38 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,7 @@
+git config --global user.name "KaiqueLimapow"
+git config --global user.email "kaiquelima1011@gmail.com"
+
+cd C:\Users\KAIQUEDELIMAALMEIDA
+git clone https://github.com/Kaiquelimapow/Caesar-Cipher.git
+cd Caesar-Cipher
+code .
